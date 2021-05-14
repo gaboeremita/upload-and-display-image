db8f734823c420a2e8d277cd27a5882117e87d95
Jovan Gabriel Ontiveros Perea <gabrieleremita@gmail.com>
2021-05-14 04:01:35 -0500

    Fix active link in navbar

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -2,8 +2,16 @@
     <div>
         <nav class="navbar sticky-top navbar-expand-lg bg-primary">
             <div class="container justify-content-center">
-                <router-link :to="{ name: 'upload-image' }" class="nav-link">Upload Image</router-link>
-                <router-link :to="{ name: 'images-list' }" class="nav-link">See Images</router-link>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'upload-image' }" class="nav-link" exact>Upload Image</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'images-list' }" class="nav-link">See Images</router-link>
+                    </li>
+                </ul>
+
+
             </div>
         </nav>
     </div>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,6 +3,7 @@ import ImagesList from "./views/ImagesList";
 
 export default {
     mode: 'history',
+    linkActiveClass: 'font-weight-bold',
     routes: [
         {
             path: '/',
